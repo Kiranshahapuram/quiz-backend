be1f0c4c29c819627471eb966c669bb93a7bef79
Kiranshahapuram <shahapuramkiran@gmail.com>
2026-03-19 19:53:47 +0530

    Fix: Suppress Celery superuser warning and force root access in celery config

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,11 @@
 import os
+import warnings
 from celery import Celery
+
+# Force root access allowed (required for some containerized environments like Railway)
+os.environ.setdefault('C_FORCE_ROOT', 'true')
+# Suppress the persistent security warning about superuser privileges
+warnings.filterwarnings("ignore", message=".*superuser privileges.*")
 
 # We don't hardcode local here so Railway environment variables take precedence.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
@@ -7,3 +13,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
